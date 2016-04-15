46ade5f51605fad26a41860a41e3f2face88954e
GARCIN David <david.garcin@epita.fr>
2016-04-15 23:30:20 +0200

    Adding docker install interactive script

---
+++ install_docker_on_debian.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+sudo_prefix=""
+docker_packages="install docker docker.io apparmor cgroup-lite"
+
+if [ "${USER}" != "root" ]; then
+    read -p "apt and usermod commands might require root right, do you want to use sudo to run them ? [Y/N] " -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Using sudo"
+        sudo_prefix="sudo"
+    else
+        echo "Skipped"
+    fi
+fi
+
+echo "Installing the following packages : $docker_packages"
+$sudo_prefix apt-get install $docker_packages || (echo "Process failed" && exit 1)
+
+read -p "Do you want to add the current user to docker group ? [Y/N] " -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Adding user $USER to docker group"
+    $sudo_prefix usermod -a -G docker $USER || (echo "Process failed" && exit 1)
+else
+    echo "Skipped"
+fi
