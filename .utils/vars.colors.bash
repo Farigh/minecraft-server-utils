29c33b7f7dd3f48ec81f0542e319203f2d9200d4
GARCIN David <david.garcin.pro@gmail.com>
2021-03-16 19:37:21 +0100

    Fixup color reset sequence
    The reset sequence was set to `\e[00m` instead of `\e[0m`
    This would cause some environment to ignore this sequence, not
    displaying the expected text formating.

--- .utils/vars.colors.bash
+++ .utils/vars.colors.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-RESET_COLOR=$(echo -en '\e[00m')
+RESET_COLOR=$(echo -en '\e[0m')
 WHITE_BOLD_COLOR=$(echo -en '\e[1;37m')
 RED_COLOR=$(echo -en '\e[0;31m')
 CYAN_COLOR=$(echo -en '\e[0;36m')
